5f8621b832a1f950e7ce4dd50f5b550d322a8d25
christinaxiong <christinaxiong@users.noreply.github.com>
2017-04-29 00:57:34 -0700

    Added basic routes, pages and buttons

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,44 @@
+class PhotosController < ApplicationController
+
+  def index
+    @list_of_all_my_photos = Photo.all
+    render("photos/index.html.erb")
+  end
+
+  def show
+    the_id=params[:da_id]
+    @my_photo = Photo.find(the_id)  #represents the row of the photo you want to display; @my_photo is an object; currently using a method action on a row object.
+    url= @my_photo.source
+    render("photos/show.html.erb")
+  end
+
+  def new_form
+    render("photos/new_form.html.erb")
+  end
+
+  def create_row
+    url = params[:da_source]
+    cap = params[:da_caption]
+    new_photo = Photo.new
+    new_photo.source= url
+    new_photo.caption = cap
+    new_photo.save
+    #render("photos/create_row.html.erb") #render goes directly to view page
+    redirect_to("/photos/#{new_photo.id}")
+  end
+
+  def edit_form
+    the_id=params[:da_id]
+    @my_photo = Photo.find(the_id) 
+
+    render("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    render("photos/update_row.html.erb")
+  end
+
+  def destroy_row
+    render("photos/destroy_row.html.erb")
+  end
+end
